cf38896d5d93bdcaf980435ea14d98b7ea95e586
Prerna <prerna@Administrators-Mac-mini.local>
2026-09-03 18:02:18 +0530

    Remove android/.cxx build files and ignore them in .gitignore

--- android/app/.cxx/RelWithDebInfo/3y306n4d/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-reanimated::reanimated)
-add_library(react-native-reanimated::reanimated SHARED IMPORTED)
-set_target_properties(react-native-reanimated::reanimated PROPERTIES
-    IMPORTED_LOCATION "D:/Apurv/Projects/Ugolf-Mobile/node_modules/react-native-reanimated/android/build/intermediates/cxx/RelWithDebInfo/4l5v64y1/obj/x86_64/libreanimated.so"
-    INTERFACE_INCLUDE_DIRECTORIES "D:/Apurv/Projects/Ugolf-Mobile/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
